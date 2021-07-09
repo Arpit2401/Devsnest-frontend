ffb53440861177cffaee2312d99a256d81a18448
Arpit Agarwal <56201652+Arpit2401@users.noreply.github.com>
2021-07-09 19:50:35 +0530

    Day 17 THA added

---
+++ day17/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import MainComponent from './Components/MainComponent/MainComponent';
+
+function App() {
+  return (
+  <><MainComponent/></>
+    )
+}
+
+export default App;
---
+++ day17/src/Caloriechart.js
@@ -0,0 +1,47 @@
+export const Caloriechart = [
+    {
+        name: "Pizza",
+        calorie: 50
+    },
+    {
+        name: "Burger",
+        calorie: 30
+    },
+    {
+        name: "Noodles",
+        calorie: 42
+    },
+    {
+        name: "Paneer",
+        calorie: 63
+    },
+    {
+        name: "Chola Bhatura",
+        calorie: 72
+    },
+    {
+        name: "Pani Puri",
+        calorie: 15
+    },
+
+    {
+        name: "Momos",
+        calorie: 26
+    },
+    {
+        name: "Chat",
+        calorie: 6
+    },
+    {
+        name: "Pav Bhaji",
+        calorie: 87
+    },
+    {
+        name: "Lassi",
+        calorie: 101
+    },
+    {
+        name: "Cold Drink",
+        calorie: 3
+    }
+]
